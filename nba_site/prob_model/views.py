456593ea085cbd4565fd676250238264224864f0
Simon Thomas <49887549+s-omas@users.noreply.github.com>
2023-08-17 17:34:15 -0600

    sidebar

--- nba_site/prob_model/views.py
+++ nba_site/prob_model/views.py
@@ -9,22 +9,22 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'all_teams': Team.objects.all()})
 
 def teams(request):
     return render(request, 'teams.html',  {'teams': Team.objects.all()})
 
 def games(request):
-    return render(request, 'games.html',  {'games': Game.objects.all()})
+    return render(request, 'games.html',  {'games': Game.objects.all(), 'all_teams': Team.objects.all()})
 
 def team_lookup(request):
     id = request.GET.get('id')
     team = Team.objects.get(team_id=id)
     schedule = Schedule.objects.get(team=team)
     games = schedule.games.all()  # Retrieve associated games
-    return render(request, 'team_lookup.html', {'team': team, 'games': games})
+    return render(request, 'team_lookup.html', {'team': team, 'games': games, 'all_teams': Team.objects.all()})
 
 def game_lookup(request):
     id = request.GET.get('id')
     game = Game.objects.get(game_id=id)
-    return render(request, 'game_lookup.html', {'game': game,})
+    return render(request, 'game_lookup.html', {'game': game, 'all_teams': Team.objects.all()})
