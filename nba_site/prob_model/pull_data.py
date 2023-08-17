001bce8537025f03f2dd20e2ca89e6222c3b2521
Simon Thomas <49887549+s-omas@users.noreply.github.com>
2023-08-17 17:10:13 -0600

    add results

--- nba_site/prob_model/pull_data.py
+++ nba_site/prob_model/pull_data.py
@@ -1,13 +1,15 @@
 import requests
 import os
-from prob_model.models import Team, Game, Schedule
+from datetime import datetime
+from prob_model.models import Team, Game, Schedule, Result
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "key.txt")
 with open(file_path, 'r') as file:
     k = file.read()
 
-testdate = "2022-02-12"
+testdate = "2022-10-15"
+test_datetime = datetime.strptime(testdate, "%Y-%m-%d")
 season = "2022"
 
 
@@ -56,14 +58,39 @@ def get_games():
             home_team = Team.objects.filter(team_id=home_id)[0]
             away_team = Team.objects.filter(team_id=away_id)[0]
             #create new game
+            start = datetime.strptime(g['date']['start'], "%Y-%m-%dT%H:%M:%S.%fZ")
+            isCompleted = (start < test_datetime)
+            print(test_datetime, g['date']['start'])
+            print(isCompleted)
+            result = None
+            # #add results if applicable
+            if isCompleted:
+                if g['scores']['visitors']['points'] > g['scores']['home']['points']:
+                    winner = away_team
+                    winner_score = g['scores']['visitors']['points']
+                    loser = home_team
+                    loser_score = g['scores']['home']['points']
+                else:
+                    winner = home_team
+                    winner_score = g['scores']['home']['points']
+                    loser = away_team
+                    loser_score = g['scores']['visitors']['points']
+                result = Result(
+                    winner = winner,
+                    loser = loser,
+                    winner_score = winner_score,
+                    loser_score = loser_score
+                )
+                result.save()
             new_game = Game(
                 game_id = g['id'],
                 home_team = home_team,
                 away_team = away_team,
                 date = g['date']['start'],
                 location = g['arena']['name'],
+                isCompleted = isCompleted,
                 prediction = None,
-                result = None
+                result = result
             )
             new_game.save()
             #add to both teams' schedule
