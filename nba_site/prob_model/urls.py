0257b81f76abd474afd2fb96656b38d6cd150619
Simon Thomas <49887549+s-omas@users.noreply.github.com>
2023-08-17 15:07:16 -0600

    team lookup page

--- nba_site/prob_model/urls.py
+++ nba_site/prob_model/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("teams", views.teams, name="teams")
+    path("teams", views.teams, name="teams"),
+    path("games", views.games, name="games"),
+    path('team_lookup/', views.team_lookup, name='team_lookup'),
 ]
--- nba_site/prob_model/views.py
+++ nba_site/prob_model/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .pull_data import *
-from .models import Team
+from .models import Team, Game
 
 # Create your views here.
 
@@ -13,3 +13,11 @@ def index(request):
 
 def teams(request):
     return render(request, 'teams.html',  {'teams': Team.objects.all()})
+
+def games(request):
+    return render(request, 'games.html',  {'games': Game.objects.all()})
+
+def team_lookup(request):
+    id = request.GET.get('id')
+    team = Team.objects.get(team_id=id)
+    return render(request, 'team_lookup.html', {'team': team})
