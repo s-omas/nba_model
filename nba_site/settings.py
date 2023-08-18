474f842a666dddd74538adb9344b82c106825044
Simon Thomas <49887549+s-omas@users.noreply.github.com>
2023-08-18 17:47:07 -0600

    restructure

--- nba_site/settings.py
+++ nba_site/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = 'nba_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
