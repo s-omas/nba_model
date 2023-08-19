1649a99fee103da22c248a84119010f1e528a795
Simon Thomas <49887549+s-omas@users.noreply.github.com>
2023-08-19 12:30:43 -0600

    proxy

--- nba_site/prob_model/apiget.py
+++ nba_site/prob_model/apiget.py
@@ -8,11 +8,6 @@ with open(file_path, 'r') as file:
     k = file.read()
 
 
-# Proxy configuration
-proxy_host = 'your_proxy_server'
-proxy_port = 8080  # Replace with your proxy server's port
-proxy_tunnel = False  # Set to True if your proxy uses tunneling, else False
-
 
 def api_get(request_string):
     #try without proxy (for local development)
@@ -28,40 +23,37 @@ def api_get(request_string):
         return data
     except:
         print("trying proxy")
-        try:
-            # Proxy configuration
-            proxy_host = 'proxy.server'
-            proxy_port = 3128  # Replace with your proxy server's port
-            proxy_tunnel = False  # Set to True if your proxy uses tunneling, else False
+        # Proxy configuration
+        proxy_host = 'proxy.server'
+        proxy_port = 3128  # Replace with your proxy server's port
+        proxy_tunnel = False  # Set to True if your proxy uses tunneling, else False
 
-            # Create a connection to the proxy server
-            conn = http.client.HTTPConnection(proxy_host, proxy_port, timeout=10)
-            endpoint = "api-nba-v1.p.rapidapi.com"
+        # Create a connection to the proxy server
+        conn = http.client.HTTPConnection(proxy_host, proxy_port, timeout=10)
+        endpoint = "api-nba-v1.p.rapidapi.com"
 
-            # Establish a connection to the target server through the proxy
-            if proxy_tunnel:
-                conn.set_tunnel(endpoint, 443)  # Replace with your target server's hostname and port
-            else:
-                conn.request("GET", request_string)
+        # Establish a connection to the target server through the proxy
+        if proxy_tunnel:
+            conn.set_tunnel(endpoint, 443)  # Replace with your target server's hostname and port
+        else:
+            conn.request("GET", request_string)
 
-            # Add headers, including those for the proxy
-            headers = {
-                'X-RapidAPI-Key': k,
-                'X-RapidAPI-Host': "api-nba-v1.p.rapidapi.com",
-            }
+        # Add headers, including those for the proxy
+        headers = {
+            'X-RapidAPI-Key': k,
+            'X-RapidAPI-Host': "api-nba-v1.p.rapidapi.com",
+        }
 
-            for key, value in headers.items():
-                conn.putheader(key, value)
+        for key, value in headers.items():
+            conn.putheader(key, value)
 
-            conn.endheaders()
+        conn.endheaders()
 
-            # Get the response from the target server through the proxy
-            res = conn.getresponse()
-            data = res.read()
-            return data
-        except:
-            print("proxy failed")
-
+        # Get the response from the target server through the proxy
+        res = conn.getresponse()
+        data = res.read()
+        return data
+    
 
 def api_get_standings(season):
     data = api_get("/standings?league=standard&season=" + season)
